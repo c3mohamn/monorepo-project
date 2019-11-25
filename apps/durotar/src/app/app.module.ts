b923382454ff115f24cd2fd47d30aab3c9dcfbd3
Nasir Mohammad <nasir.m23@hotmail.com>
2019-11-25 15:33:55 -0500

    Add service worker

--- apps/durotar/src/app/app.module.ts
+++ apps/durotar/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { DurotarComponent } from './durotar/durotar.component';
 import { CoreModule } from '@monorepo-project/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, DurotarComponent],
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), HttpClientModule, AppRoutingModule, 
-  CoreModule],
+  CoreModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
